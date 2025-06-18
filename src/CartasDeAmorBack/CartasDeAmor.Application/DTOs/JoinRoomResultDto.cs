2af3be9d9d1caceefc1d82ca4aef83264f03fab8
Lucas Cardoso <lucascardoso910@gmail.com>
2025-06-18 11:53:12 -0300

    fix: add game room to JoinRoomResultDto
    Signed-off-by: Lucas Cardoso <lucascardoso910@gmail.com>

--- src/CartasDeAmorBack/CartasDeAmor.Application/DTOs/JoinRoomResultDto.cs
+++ src/CartasDeAmorBack/CartasDeAmor.Application/DTOs/JoinRoomResultDto.cs
@@ -5,6 +5,7 @@ namespace CartasDeAmor.Application.DTOs;
 public class JoinRoomResultDto
 {
     public string RoomId { get; set; } = string.Empty;
+    public string RoomName { get; set; } = string.Empty;
     public int PlayerId { get; set; }
     public string HostEmail { get; set; } = string.Empty;
     public ICollection<string> Players { get; set; } = [];
@@ -12,6 +13,7 @@ public class JoinRoomResultDto
     public JoinRoomResultDto(Game game, Player player)
     {
         RoomId = game.Id.ToString();
+        RoomName = game.Name;
         PlayerId = player.Id;
         HostEmail = game.HostEmail;
         Players = game.Players.Select(p => p.UserEmail).ToList();
